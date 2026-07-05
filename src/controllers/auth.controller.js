b49f653265657740f7852d9b9fb952cdb14a21f0
Ujjwal Tyagi <ujjwaltyagivashishtha@gmail.com>
2026-07-05 12:49:48 +0530

    feat: integrate email service to send registration emails upon user registration

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/user.model");
 const jwt = require("jsonwebtoken");
+const emailService = require("../services/email.service");
 
 /**
  * User Registration Controller
@@ -62,6 +63,8 @@ async function userRegister(req, res) {
       },
       token,
     });
+
+    await emailService.sendRegistrationEmail(user.email,user.name);
 
   } catch (error) {
     console.error(error);
