e5bd6c0dbc6e24e89fd47761e0c3b57e44162a62
Ujjwal Tyagi <ujjwaltyagivashishtha@gmail.com>
2026-07-07 10:27:15 +0530

    feat: add account router to handle account-related routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 
+
+/**
+ * Routes for the application
+ */
+
 const authRouter = require('./routes/auth.routes');
+const accountRouter = require('./routes/account.routes');
 
 const app = express();
 app.use(cookieParser());
@@ -9,7 +15,13 @@ app.use(cookieParser());
 app.use(express.json());
 
 
+
+/**
+ * Base route for authentication and account management
+ */
 app.use("/api/auth", authRouter);
+app.use("/api/accounts", accountRouter);
+
 
 
 
