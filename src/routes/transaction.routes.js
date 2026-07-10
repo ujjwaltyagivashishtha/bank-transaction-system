2939c288813f93f197e6a619df71d16781746fb2
Ujjwal Tyagi <ujjwaltyagivashishtha@gmail.com>
2026-07-10 19:37:16 +0530

    feat: add transaction creation endpoint with authentication middleware

--- src/routes/transaction.routes.js
+++ src/routes/transaction.routes.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 const authMiddleware = require('../middleware/auth.middleware');
+const transactionController = require('../controllers/transaction.controller');
 
 
 const transactionRouter = Router();
 
 
 
-transactionRouter.post("/",authMiddleware.authMiddleware,);
+transactionRouter.post("/",authMiddleware.authMiddleware,transactionController.createTransaction);
 
 
 module.exports = transactionRouter;
