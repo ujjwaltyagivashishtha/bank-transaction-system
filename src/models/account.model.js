0b5bc2976d800fbed663fd71dca44bbf5cd37a9e
Ujjwal Tyagi <ujjwaltyagivashishtha@gmail.com>
2026-07-10 18:58:20 +0530

    feat: add getBalance method to account model for calculating account balance

--- src/models/account.model.js
+++ src/models/account.model.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const ledgerModel = require("./ledger.model");
+
 
 const accountSchema = new mongoose.Schema({
   user: {
@@ -25,6 +27,31 @@ const accountSchema = new mongoose.Schema({
 });
 
 accountSchema.index({ user: 1, status: 1 });
+
+
+accountSchema.methods.getBalance = async function(){
+  const balanceData = await ledgerModel.aggregate([
+    { $match: { account: this._id } },
+    { $group: {
+      _id: null,
+      totalCredit: { $sum: { $cond: [ { $eq: [ "$type", "CREDIT" ] }, "$amount", 0 ] } },
+      totalDebit: { $sum: { $cond: [ { $eq: [ "$type", "DEBIT" ] }, "$amount", 0 ] } }
+    }},
+    { $project: {
+      _id: 0,
+      balance: { $subtract: [ "$totalCredit", "$totalDebit" ] }   
+    }
+    }
+
+
+  ]);
+
+  if (!balanceData || balanceData.length === 0) {
+    return 0;
+  }
+
+  return balanceData[0].balance;
+}
 
 const accountModel = mongoose.model("Account",accountSchema);
 
