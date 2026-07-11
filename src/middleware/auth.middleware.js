3c481bee66321157eb6e6ee1d4a7e0e0d27f562d
Ujjwal Tyagi <ujjwaltyagivashishtha@gmail.com>
2026-07-11 14:04:54 +0530

    feat: add authSystemUserMiddleware for system user authentication

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -31,6 +31,40 @@ async function authMiddleware(req, res, next){
 }
 
 
+async function authSystemUserMiddleware(req, res, next){
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+  
+  if(!token){
+    return res.status(401).json({
+      message: "Unauthorized Access: No token provided",
+    });
+  }
+
+  try{
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await userModel.findById(decoded.userId).select('+systemUser');
+
+    if(!user.systemUser){
+      return res.status(403).json({
+        message: "Forbidden Access: User is not a system user"
+      })
+    }
+
+    req.user = user;
+    return next();
+  } catch (error) {
+    console.error(error);
+    return res.status(401).json({
+      message: "Unauthorized Access: Invalid token",
+    });
+  }
+
+
+
+}
+
 module.exports = {
-  authMiddleware
+  authMiddleware,
+  authSystemUserMiddleware
 }
