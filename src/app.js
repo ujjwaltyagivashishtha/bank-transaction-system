c9fa2f1e4d0a07734f9f3efddd65f725f0ea4162
Ujjwal Tyagi <ujjwaltyagivashishtha@gmail.com>
2026-07-11 15:17:36 +0530

    refactor: improve code structure and formatting in app.js

--- src/app.js
+++ src/app.js
@@ -1,28 +1,31 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
+const express = require("express")
+const cookieParser = require("cookie-parser")
 
 
-/**
- * Routes for the application
- */
 
-const authRouter = require('./routes/auth.routes');
-const accountRouter = require('./routes/account.routes');
-
-const app = express();
-app.use(cookieParser());
-
-app.use(express.json());
+const app = express()
 
 
+app.use(express.json())
+app.use(cookieParser())
 
 /**
- * Base route for authentication and account management
+ * - Routes required
  */
-app.use("/api/auth", authRouter);
-app.use("/api/accounts", accountRouter);
+const authRouter = require("./routes/auth.routes")
+const accountRouter = require("./routes/account.routes")
+const transactionRoutes = require("./routes/transaction.routes")
 
+/**
+ * - Use Routes
+ */
 
+app.get("/", (req, res) => {
+    res.send("Ledger Service is up and running")
+})
 
+app.use("/api/auth", authRouter)
+app.use("/api/accounts", accountRouter)
+app.use("/api/transactions", transactionRoutes)
 
-module.exports = app;
+module.exports = app
