96a1f218ec8890167fd34c983803c84c317e2a88
Ujjwal Tyagi <ujjwaltyagivashishtha@gmail.com>
2026-07-11 15:15:31 +0530

    refactor: update auth routes for consistency and improve documentation

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,21 @@
-const express = require('express');
-const authController = require('../controllers/auth.controller');
+const express = require("express")
+const authController = require("../controllers/auth.controller")
 
-const router = express.Router();
-
-router.post('/register', authController.userRegister);
-router.post('/login',authController.userLogin);
+const router = express.Router()
 
 
+/* POST /api/auth/register */
+router.post("/register", authController.userRegisterController)
 
 
-module.exports = router;
+/* POST /api/auth/login */
+router.post("/login",authController.userLoginController)
+
+/**
+ * - POST /api/auth/logout
+ */
+router.post("/logout", authController.userLogoutController)
+
+
+
+module.exports = router
